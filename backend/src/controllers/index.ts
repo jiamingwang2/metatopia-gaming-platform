1ff54ab9c1fee03465779b111ac1430db7599731
jiamingwang2 <chc2662@163.com>
2025-08-11 12:44:03 +0800

    Add backend config, controllers, middleware and services index files

---
+++ backend/src/controllers/index.ts
@@ -0,0 +1,15 @@
+import { NewsController } from './newsController';
+import { TeamController } from './teamController';
+import { RoadmapController } from './roadmapController';
+import { ContactController } from './contactController';
+import { TokenController } from './tokenController';
+
+export { NewsController, TeamController, RoadmapController, ContactController, TokenController };
+
+export default {
+  NewsController,
+  TeamController,
+  RoadmapController,
+  ContactController,
+  TokenController,
+};
---
+++ backend/src/services/index.ts
@@ -0,0 +1,15 @@
+import { NewsService } from './newsService';
+import { TeamService } from './teamService';
+import { RoadmapService } from './roadmapService';
+import { ContactService } from './contactService';
+import { TokenService } from './tokenService';
+
+export { NewsService, TeamService, RoadmapService, ContactService, TokenService };
+
+export default {
+  NewsService,
+  TeamService,
+  RoadmapService,
+  ContactService,
+  TokenService,
+};
